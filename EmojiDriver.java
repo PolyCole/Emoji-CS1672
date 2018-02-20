a6324f95bce31f1caaa5a326d6e9c0fd841e2b34
Cole Polyak <colepolyak@gmail.com>
2018-02-19 20:50:44 -0700

    Added to driver so that invalid file inputs are better handled. Added file to package.

--- EmojiDriver.java
+++ EmojiDriver.java
@@ -1,3 +1,5 @@
+package emoji;
+
 // Import block
 import edu.princeton.cs.introcs.StdDraw;
 import java.io.FileInputStream;
@@ -15,21 +17,20 @@ import java.util.Scanner;
  */
 
 public class EmojiDriver {
-
+	
+	public static Scanner file = null;
+	
 	public static void main(String[] args) {
 		
-		// Scanner initialization and try/catch block.
-		Scanner file = null;
+		// Inputting file
+		Scanner keyboard = new Scanner(System.in);
 		
-		try
-		{
-			file = new Scanner(new FileInputStream("emojiInput.txt"));
-		}
-		catch(FileNotFoundException e)
-		{
-			System.out.println("File not found. Exiting...");
-			System.exit(0);
-		}
+		System.out.println("Please input file name: ");
+		initFile(keyboard.next());
+		System.out.println("File success. Reading file...");
+		
+		keyboard.close();
+		
 		
 		// Reads array size from file.
 		int rows = file.nextInt();
@@ -59,11 +60,11 @@ public class EmojiDriver {
 				String currentObject = file.next();
 				
 				// Creates emoji based on file dictation.
-				if(currentObject.equals("smile"))
+				if("smile".equals(currentObject))
 				{
 					arr[i][j] = new SmileyFaceEmoji(xPos + emojiSize, yPos - emojiSize, emojiSize);
 				}
-				else if(currentObject.equals("frown"))
+				else if("frown".equals(currentObject))
 				{
 					arr[i][j] = new FrowningFaceEmoji(xPos + emojiSize, yPos - emojiSize, emojiSize);
 				}
@@ -89,26 +90,32 @@ public class EmojiDriver {
 				{
 					
 					arr[i][j].draw();
-					arr[i][j].animate(); // Much simpler with abstract methods.
-					
-					// Uncomment lines 94-103 for old method of animation.
-					
-//					if(arr[i][j] instanceof FaceEmoji)
-//					{
-//						FaceEmoji a = (FaceEmoji)arr[i][j];
-//						a.wink();
-//					}
-//					else
-//					{
-//						Clock a = (Clock)arr[i][j];
-//						a.tick();
-//					}
-					
+					arr[i][j].animate(); 				
 				}
 			}
 			StdDraw.show();
 			StdDraw.pause(500);
 		}
+	}
+	
+	// Method goes about establishing file input.
+	public static String initFile(String fileName)
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		try
+		{
+			file = new Scanner(new FileInputStream(fileName));
+		}
+		catch(FileNotFoundException e)
+		{
+			// Infinite until valid file is inputted.
+			System.out.println("File not found. Please double check filename and try again.");
+			initFile(keyboard.next());
+		}
+		
+		keyboard.close();
+		return fileName;
 	}
 
 }
